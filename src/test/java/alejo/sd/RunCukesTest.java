36a122a40bf3293e5c69c70471b48b674f2fd11e
Alejosd <alejosd5@gmail.com>
2016-07-31 20:06:35 -0400

    change path features in class RunCukesTest

--- src/test/java/alejo/sd/RunCukesTest.java
+++ src/test/java/alejo/sd/RunCukesTest.java
@@ -6,7 +6,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-		features = "classpath:./resources/features",
+		features = "./src/test/java/resources/features",
 		plugin = {"pretty", "html:target/cucumber"}
 		)
 public class RunCukesTest {
